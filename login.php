2a12bef67ac5a378667b6ee6ea2092d04af72493
lpitto86 <luca.p481@gmail.com>
2025-01-13 17:02:23 +0100

    Login

---
+++ login.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+      <meta charset="UTF-8">
+      <meta name="viewport" content="width=device-width, initial-scale=1.0">
+      <title>Login</title>
+
+</head>
+<body>
+      <header>
+            Login
+      </header>
+      <main>
+            <form action="profile.php" method="POST">
+                  <div>
+                        <label for="email">
+                              Email
+                        </label>
+                        <div>
+                              <input type="email" name="email" id="email">
+                        </div>
+                  </div>
+                  <div>
+                        <label for="password">
+                              Password
+                        </label>
+                        <div>
+                              <input type="password" name="password" id="password">
+                        </div>
+                  </div>
+                  <div>
+                        <button type="submit">
+                              Accedi
+                        </button>
+                  </div>
+            </form>
+      </main>
+      <footer>
+      </footer>
+</body>
+</html>
